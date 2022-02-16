037fdb4871e073d1280e8b68f323be833e046429
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-02-15 22:03:42 -0300

    Criação de nosso primeiro projeto winforms

--- Primeiro Projeto WinForms/Primeiro Projeto WinForms/FrmPrincipal.Designer.cs
+++ Primeiro Projeto WinForms/Primeiro Projeto WinForms/FrmPrincipal.Designer.cs
@@ -29,31 +29,45 @@
         private void InitializeComponent()
         {
             this.lbNome = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btConfirma = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lbNome
             // 
             this.lbNome.AutoSize = true;
+            this.lbNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbNome.Location = new System.Drawing.Point(12, 115);
             this.lbNome.Name = "lbNome";
-            this.lbNome.Size = new System.Drawing.Size(189, 25);
+            this.lbNome.Size = new System.Drawing.Size(283, 37);
             this.lbNome.TabIndex = 0;
             this.lbNome.Text = "Informe seu nome:";
             // 
-            // textBox1
+            // txtNome
             // 
-            this.textBox1.Location = new System.Drawing.Point(207, 112);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(306, 31);
-            this.textBox1.TabIndex = 1;
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(301, 112);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(306, 44);
+            this.txtNome.TabIndex = 1;
+            // 
+            // btConfirma
+            // 
+            this.btConfirma.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btConfirma.Location = new System.Drawing.Point(19, 174);
+            this.btConfirma.Name = "btConfirma";
+            this.btConfirma.Size = new System.Drawing.Size(588, 69);
+            this.btConfirma.TabIndex = 2;
+            this.btConfirma.Text = "Confirma";
+            this.btConfirma.UseVisualStyleBackColor = true;
             // 
             // FrmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(892, 610);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(708, 352);
+            this.Controls.Add(this.btConfirma);
+            this.Controls.Add(this.txtNome);
             this.Controls.Add(this.lbNome);
             this.Name = "FrmPrincipal";
             this.Text = "Meu Primeiro Form";
@@ -65,7 +79,8 @@
         #endregion
 
         private System.Windows.Forms.Label lbNome;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btConfirma;
     }
 }
 
