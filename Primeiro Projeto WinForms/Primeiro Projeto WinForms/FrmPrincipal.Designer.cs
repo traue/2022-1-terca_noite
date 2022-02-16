8af7a6cfed4cd9167d618e1ec93e3b22c3d6c538
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-02-15 21:56:16 -0300

    Criação do primeiro projeto

---
+++ Primeiro Projeto WinForms/Primeiro Projeto WinForms/FrmPrincipal.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Primeiro_Projeto_WinForms
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNome = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbNome
+            // 
+            this.lbNome.AutoSize = true;
+            this.lbNome.Location = new System.Drawing.Point(12, 115);
+            this.lbNome.Name = "lbNome";
+            this.lbNome.Size = new System.Drawing.Size(189, 25);
+            this.lbNome.TabIndex = 0;
+            this.lbNome.Text = "Informe seu nome:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(207, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(306, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 610);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbNome);
+            this.Name = "FrmPrincipal";
+            this.Text = "Meu Primeiro Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbNome;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
