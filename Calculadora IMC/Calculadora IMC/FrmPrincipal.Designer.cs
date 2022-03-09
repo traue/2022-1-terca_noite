6a52987552c6c27bc9d3cbb05c004b2ca79fab03
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-03-08 21:52:32 -0300

    Projeto - Calculadora IMC

---
+++ Calculadora IMC/Calculadora IMC/FrmPrincipal.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Calculadora_IMC
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.lbPeso = new System.Windows.Forms.Label();
+            this.lbResultadoIMC = new System.Windows.Forms.Label();
+            this.lbClassificacaoIMC = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.btCalcular = new System.Windows.Forms.Button();
+            this.lbAltura = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbPeso
+            // 
+            this.lbPeso.AutoSize = true;
+            this.lbPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPeso.Location = new System.Drawing.Point(12, 61);
+            this.lbPeso.Name = "lbPeso";
+            this.lbPeso.Size = new System.Drawing.Size(205, 29);
+            this.lbPeso.TabIndex = 0;
+            this.lbPeso.Text = "Informe seu peso:";
+            // 
+            // lbResultadoIMC
+            // 
+            this.lbResultadoIMC.AutoSize = true;
+            this.lbResultadoIMC.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResultadoIMC.Location = new System.Drawing.Point(12, 306);
+            this.lbResultadoIMC.Name = "lbResultadoIMC";
+            this.lbResultadoIMC.Size = new System.Drawing.Size(61, 29);
+            this.lbResultadoIMC.TabIndex = 2;
+            this.lbResultadoIMC.Text = "------";
+            // 
+            // lbClassificacaoIMC
+            // 
+            this.lbClassificacaoIMC.AutoSize = true;
+            this.lbClassificacaoIMC.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbClassificacaoIMC.Location = new System.Drawing.Point(12, 394);
+            this.lbClassificacaoIMC.Name = "lbClassificacaoIMC";
+            this.lbClassificacaoIMC.Size = new System.Drawing.Size(61, 29);
+            this.lbClassificacaoIMC.TabIndex = 3;
+            this.lbClassificacaoIMC.Text = "------";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPeso.Location = new System.Drawing.Point(223, 55);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(148, 35);
+            this.txtPeso.TabIndex = 4;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAltura.Location = new System.Drawing.Point(223, 122);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(148, 35);
+            this.txtAltura.TabIndex = 5;
+            // 
+            // btCalcular
+            // 
+            this.btCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCalcular.Location = new System.Drawing.Point(17, 194);
+            this.btCalcular.Name = "btCalcular";
+            this.btCalcular.Size = new System.Drawing.Size(354, 55);
+            this.btCalcular.TabIndex = 6;
+            this.btCalcular.Text = "Calcular IMC";
+            this.btCalcular.UseVisualStyleBackColor = false;
+            // 
+            // lbAltura
+            // 
+            this.lbAltura.AutoSize = true;
+            this.lbAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAltura.Location = new System.Drawing.Point(12, 128);
+            this.lbAltura.Name = "lbAltura";
+            this.lbAltura.Size = new System.Drawing.Size(209, 29);
+            this.lbAltura.TabIndex = 7;
+            this.lbAltura.Text = "Informe sua altura:";
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 530);
+            this.Controls.Add(this.lbAltura);
+            this.Controls.Add(this.btCalcular);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.lbClassificacaoIMC);
+            this.Controls.Add(this.lbResultadoIMC);
+            this.Controls.Add(this.lbPeso);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmPrincipal";
+            this.Text = "Calculadora IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbPeso;
+        private System.Windows.Forms.Label lbResultadoIMC;
+        private System.Windows.Forms.Label lbClassificacaoIMC;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Button btCalcular;
+        private System.Windows.Forms.Label lbAltura;
+    }
+}
+
---
+++ Calculadora IMC/Calculadora IMC/Imc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_IMC
+{
+    public class Imc
+    {
+        private float peso;
+        private float altura;
+
+        public float Peso
+        {
+            get => peso;  //retorno, famoso "me dá"
+            set => peso = value;  //altera o valor, famoco "tó"
+        }
+
+        public float Altura
+        {
+            get => altura;
+            set => altura = value;
+        }
+
+        public float calculaIMC()
+        {
+            float imc = peso / (altura * altura);
+            return imc;
+        }
+
+        //desafio do tio: 
+        //façam sozinhos o método que calcula a classificação...
+        //usando essa tabela: https://arquivos.sbn.org.br/equacoes/eq5.htm
+        //❤️
+
+    }
+}
