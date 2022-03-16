d3c81217bfbd7c8b5b9329bd430d2000a4c30646
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-03-15 22:00:46 -0300

    Criação do projeto de Bhaskara

---
+++ Bhaskara/Bhaskara/FrmPrincipal.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Bhaskara
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.lbInformacao = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbInformacao
+            // 
+            this.lbInformacao.AutoSize = true;
+            this.lbInformacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbInformacao.Location = new System.Drawing.Point(12, 36);
+            this.lbInformacao.Name = "lbInformacao";
+            this.lbInformacao.Size = new System.Drawing.Size(468, 37);
+            this.lbInformacao.TabIndex = 0;
+            this.lbInformacao.Text = "Insira uma equação quadrática:";
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbInformacao);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmPrincipal";
+            this.Text = "Bhaskara";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbInformacao;
+    }
+}
