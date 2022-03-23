1e9a019f6168367e50be463ae174d72af8f2d16f
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-03-22 21:58:23 -0300

    Continuação do projeto

---
+++ Bhaskara/Bhaskara/Bhaskara.cs
@@ -0,0 +1,34 @@
+﻿namespace Bhaskara
+{
+    public class Bhaskara
+    {
+        private int a, b, c;
+
+        public int A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public int B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public int C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        public double calculaDelta()
+        {
+            double delta = (b * b) - 4 * a * c;
+            return delta;
+        }
+
+        //paramos aqui... 
+        //preparar uma aula de lógica :D
+    }
+}
